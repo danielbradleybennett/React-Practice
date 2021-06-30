1baead2e8a156a7a22bb27e6dca284aa6bb0822a
DanielBradleyBennett <danielbradleybennett@gmail.com>
2021-06-29 21:20:02 -0500

    practice

--- src/components/ApplicationView.js
+++
@@ -1,3 +0,0 @@
-import React from "react"
-{Route} from "react-router-dom"
-import {Button} from "react-bootstrap"
---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,4 @@
+import React from "react"
+import { Route } from 'react-router-dom'
+// import { Button } from "react-bootstrap"
+import "./ApplicationView.css"
