0a0a77fdb0041c0711d488217ea2247d0c93e3b5
DanielBradleyBennett <danielbradleybennett@gmail.com>
2021-07-05 14:05:53 -0500

    login shows

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from "react"
 import { Route, Redirect } from "react-router-dom"
 // import ApplicationViews from "./ApplicationViews"
 // import NavBar from "./nav/NavBar"
-// import Login from "./auth/Login"
-// import Register from "./auth/Register"
+import Login from "./Auth/Login"
+import Register from "./Auth/Register"
 import "./App.css"
 
 export default () => (
@@ -21,8 +21,8 @@ export default () => (
       }
     }} />
 
-    {/* <Route path="/login" render={props => <Login {...props} />} /> */}
-    {/* <Route path="/register" render={props => <Register {...props} />} /> */}
+    <Route path="/login" render={props => <Login {...props} />} />
+    <Route path="/register" render={props => <Register {...props} />} />
   </>
 )
 
