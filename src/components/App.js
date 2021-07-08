8bb14ad237d544f5df35670d48a98a613d1ce1f7
DanielBradleyBennett <danielbradleybennett@gmail.com>
2021-07-07 23:16:33 -0500

    removed login

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,21 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 // import ApplicationViews from "./ApplicationViews"
-// import NavBar from "./nav/NavBar"
-import Login from "./Auth/Login"
-import Register from "./Auth/Register"
+import NavBar from "./Nav/NavBar"
 import "./App.css"
 
 export default () => (
   <>
     <Route render={() => {
-      if (localStorage.getItem("currentUserId")) {
-        return (
-          <>
-            {/* <Route render={props => <NavBar {...props} />} /> */}
-            {/* <Route render={props => <ApplicationViews {...props} />} /> */}
-          </>
-        )
-      } else {
-        return <Redirect to="/login" />
-      }
+      return (
+        <>
+          <Route render={props => <NavBar {...props} />} />
+          {/* <Route render={props => <ApplicationViews {...props} />} /> */}
+        </>
+      )
+
     }} />
 
-    <Route path="/login" render={props => <Login {...props} />} />
-    <Route path="/register" render={props => <Register {...props} />} />
   </>
 )
 
